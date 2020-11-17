1efe27ed4fda0f55a6b738c69d8384ca2517e236
yuki526 <yk.g187@gmail.com>
2020-11-17 19:30:32 +0900

    ストロングパラメーター設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,14 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
 
   end
+
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :price, :detail, :category_id, :item_status_id, :delivery_fee_id, :prefecture_id, :required_day_id).merge(user: current_user_id)
+  end
+  
 end
