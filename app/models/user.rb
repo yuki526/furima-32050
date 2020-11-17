f02ae60e250820843cd10e74a3b174645b8a4256
yuki526 <yk.g187@gmail.com>
2020-11-17 12:28:11 +0900

    Userモデルのバリデーション記載

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :orders
+
+  with_options presence: true do
+    validates :email, uniqueness: true
+    validates :password, format: {with: /\A(?=.*?[a-z])[a-z\d]+\z/, message: "Include both letters and numbers"}
+    validates :nickname
+    validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+    validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+    validates :first_name_reading format: { with: /\A[ァ-ヶー－]+\z/, message: " Full-width katakana characters"}
+    validates :last_name_reading format: { with: /\A[ァ-ヶー－]+\z/, message: " Full-width katakana characters"}
+    validates :birth_date
+  end
 end
