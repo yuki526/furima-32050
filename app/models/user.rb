56eb6ef4fbc960a5fbea5761185bbd6104f4a64b
yuki526 <yk.g187@gmail.com>
2020-11-17 12:58:09 +0900

    新規登録・ログイン機能をとりあえず動かせる状態

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,8 @@ class User < ApplicationRecord
     validates :nickname
     validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
     validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
-    validates :first_name_reading format: { with: /\A[ァ-ヶー－]+\z/, message: " Full-width katakana characters"}
-    validates :last_name_reading format: { with: /\A[ァ-ヶー－]+\z/, message: " Full-width katakana characters"}
+    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: " Full-width katakana characters"}
+    validates :last_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: " Full-width katakana characters"}
     validates :birth_date
   end
 end
