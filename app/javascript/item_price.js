a45b3dca735a181c674406e4442b28ebc98ffe5f
yuki526 <yk.g187@gmail.com>
2020-11-18 15:47:12 +0900

    手数料・利益表示の実装（JS）

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,5 +1,19 @@
-function item_price (){
-  console.log("非同期投稿を実装");
+function itemPrice (){
+  const price = document.getElementById("item-price");
+  price.addEventListener("keyup", () => {
+    // 販売手数料と利益を計算(手数料は小数点以下切り捨て)
+    const charge = Math.floor(price.value * 0.1);
+    const profit = price.value - charge;
+
+    // 販売手数料と利益の要素を取得
+    const showCharge = document.getElementById("add-tax-price")
+    const showProfit = document.getElementById("profit")
+
+    // 置き換え
+    showCharge.innerHTML = `${charge}`
+    showProfit.innerHTML = `${profit}`
+    
+  });
 }
 
-window.addEventListener('load', item_price);
+window.addEventListener('load', itemPrice);
