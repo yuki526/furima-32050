fdea41dd61b8940a000a57e6232006a078ba8e74
yuki526 <yk.g187@gmail.com>
2020-11-18 16:17:50 +0900

    itemモデルのFactoryBotを定義

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
+    name { 'テスト商品' }
+    price { 5000 }
+    detail { 'これはテスト商品です。' }
+    category_id { 3 }
+    item_status_id { 3 }
+    delivery_fee_id { 3 }
+    prefecture_id { 3 }
+    required_day_id { 3 }
     
   end
 end
