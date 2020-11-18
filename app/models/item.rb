9036ab320b106988de01ada034fef6bfcf573aa6
yuki526 <yk.g187@gmail.com>
2020-11-18 15:02:06 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,15 +11,19 @@ class Item < ApplicationRecord
   belongs_to :required_day
 
   with_options presence: true do
-    validates :name
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range" }
-    validates :detail
-    validates :category_id, numericality: { other_than: 1 } 
-    validates :item_status_id, numericality: { other_than: 1 } 
-    validates :delivery_fee_id, numericality: { other_than: 1 } 
-    validates :prefecture_id, numericality: { other_than: 0 } 
-    validates :required_day_id, numericality: { other_than: 1 } 
     validates :image
+    validates :name
+    validates :detail
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range" }, format: { with: /\A\d{3,7}\z/, message: 'Half-width number' }
+    validates :category_id, numericality: { other_than: 1, message: "Select" } 
+    validates :item_status_id, numericality: { other_than: 1, message: "Select" } 
+    validates :delivery_fee_id, numericality: { other_than: 1, message: "Select" } 
+    validates :prefecture_id, numericality: { other_than: 0, message: "Select" } 
+    validates :required_day_id, numericality: { other_than: 1, message: "Select" } 
   end
 
 end
+
+
+# numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range" },
+# validates :price,  format: { with: /\A[0-9]+\z/, message: 'Half-width number' }
