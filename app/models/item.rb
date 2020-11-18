7aba1e3c5d3eff66182e8d287fb1cf85c639268b
yuki526 <yk.g187@gmail.com>
2020-11-18 11:16:37 +0900

    categoryモデルの定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,7 @@ class Item < ApplicationRecord
   belings_to :user
   has_one :order
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
 end
