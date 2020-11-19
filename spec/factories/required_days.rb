5c7911e4617cf27401fcef7176ddd635a3c74ff9
yuki526 <yk.g187@gmail.com>
2020-11-19 11:19:15 +0900

    不要なspecとFactoryBotのファイルを削除

--- spec/factories/required_days.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :required_day do
-    
-  end
-end
--- spec/models/required_day_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe RequiredDay, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
