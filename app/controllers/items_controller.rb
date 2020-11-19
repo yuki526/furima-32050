2db1beff015a7a1a4c9ae8cf67fe7db9bf69db61
yuki526 <yk.g187@gmail.com>
2020-11-19 18:31:35 +0900

    メソッド仮置き

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,6 +31,13 @@ before_action :authenticate_user!, only: [:new, :create, :destroy]
     redirect_to root_path
   end
 
+  def edit
+
+  end
+
+  def update
+    
+  end
 
   private
   def item_params
