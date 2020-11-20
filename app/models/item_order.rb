1d0b21f6d230ff36d21c9645979bf18a30f8d9f1
yuki526 <yk.g187@gmail.com>
2020-11-20 16:49:20 +0900

    formオブジェクトのメソッドを記述

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,4 +1,11 @@
 class ItemOrder
   include ActiveModel::Model
   attr_accessor :user. :item, :order, :postal_code, :prefeture_id, :city, :house_number, :building_name, :telephone_number
+
+  # バリデーション未設定
+
+  def save
+    order = Order.create(user: params[:user], item: params[:item])
+    Address.create(postal_code: postal_code, prefeture_id: prefeture_id, city: city, house_number: house_number, building_name: building_name, telephone_number: telephone_number, order: order)
+  end
 end
