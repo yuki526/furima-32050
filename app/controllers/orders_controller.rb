a3ead6700bbc3bd9cc7bcff235c5b8aac3267969
yuki526 <yk.g187@gmail.com>
2020-11-20 17:38:32 +0900

    購入機能のベース実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,24 @@
 class OrdersController < ApplicationController
   def index
-    
+    @item = Item.find(params[:item_id])
+    @item_order = ItemOrder.new
+  end
+
+  def create
+    @item_order = ItemOrder.new(order_params)
+    if @item_order.valid?
+      @item_order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+
+  private
+  def order_params
+    params.require(:item_order).permit(:postal_code, :prefecture_id, :city, 
+                                      :house_number, :building_name, :telephone_number
+                                      ).merge(user_id: current_user.id, item_id: params[:item_id])   
   end
 end
--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,11 +1,13 @@
 class ItemOrder
   include ActiveModel::Model
-  attr_accessor :user. :item, :order, :postal_code, :prefeture_id, :city, :house_number, :building_name, :telephone_number
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number
 
   # バリデーション未設定
 
   def save
-    order = Order.create(user: params[:user], item: params[:item])
-    Address.create(postal_code: postal_code, prefeture_id: prefeture_id, city: city, house_number: house_number, building_name: building_name, telephone_number: telephone_number, order: order)
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, 
+                    city: city, house_number: house_number, building_name: building_name, 
+                    telephone_number: telephone_number, order_id: order.id)
   end
 end
