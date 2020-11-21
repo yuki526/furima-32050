015d2bb478302e7f50b27554412af5a1371e26ee
yuki526 <yk.g187@gmail.com>
2020-11-21 14:22:13 +0900

    サーバーサイド実装

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,6 +1,6 @@
 class ItemOrder
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number, :token
+  attr_accessor :user, :item, :postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly"}
@@ -8,10 +8,11 @@ class ItemOrder
     validates :city
     validates :house_number
     validates :telephone_number, numericality: { message: "Input only number" }
+    validates :token
   end
 
   def save
-    order = Order.create(item_id: item_id, user_id: user_id)
+    order = Order.create(item_id: item, user_id: user)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, 
                     city: city, house_number: house_number, building_name: building_name, 
                     telephone_number: telephone_number, order_id: order.id)
