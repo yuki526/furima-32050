4154117404bcd9fe41927e37e9eea3a47a9f4116
yuki526 <yk.g187@gmail.com>
2020-11-21 14:24:45 +0900

    エラーメッセージの出力

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -3,12 +3,12 @@ class ItemOrder
   attr_accessor :user, :item, :postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number, :token
 
   with_options presence: true do
+    validates :token
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly"}
     validates :prefecture_id, numericality: { other_than: 0, message: "Select" }
     validates :city
     validates :house_number
     validates :telephone_number, numericality: { message: "Input only number" }
-    validates :token
   end
 
   def save
