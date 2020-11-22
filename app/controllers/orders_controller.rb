82313af8138749d1fbe473f2dfd7114eac142553
yuki526 <yk.g187@gmail.com>
2020-11-22 11:18:04 +0900

    不要な記述の削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -29,10 +29,9 @@ class OrdersController < ApplicationController
   end
 
   def item_pay
-    item = Item.find(@item_order.item)
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: item.price,  # 商品の値段
+      amount: @item.price,  # 商品の値段
       card: order_params[:token],    # カードトークン
       currency: 'jpy'                 # 通貨の種類（日本円）
     )
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'items#index'
 
   resources :items do
-    resources :orders, only: [:index, :new, :create]
+    resources :orders, only: [:index, :create]
   end
 end
